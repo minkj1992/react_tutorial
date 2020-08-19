e41e458f0b412a8d223146cfd281d739052e911d
minkj1992 <minkj1992@gmail.com>
2020-08-19 22:59:38 +0900

    Update: 승자 결정하기

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -31,6 +31,11 @@ class Board extends React.Component {
         // 배열 복사본 사용 .slice() -> 되돌아가기 가능(reuse)
         // react는 immutable 사용시, 데이터 변경을 감지하여 자동 렌더링 합니다.
         const squares = this.state.squares.slice();
+
+        if (calculateWinner(squares) || squares[i]){
+            return;
+        }
+
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
             squares: squares,
@@ -48,7 +53,14 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: ' + (this.state.xIsNext ? 'X': 'O');
+        const winner = calculateWinner(this.state.squares);
+        let status;
+
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
 
         return (
             <div>
@@ -89,9 +101,30 @@ class Game extends React.Component {
     }
 }
 
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
+
 // ========================================
 
 ReactDOM.render(
     <Game />,
     document.getElementById('root')
 );
+
