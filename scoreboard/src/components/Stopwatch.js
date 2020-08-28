7fdeb8afd63c2508f8812d6b6f70df80d3f484c6
leoo.j <leoo.j@kakaocorp.com>
2020-08-28 21:32:09 +0900

    Add: stopwatch.js 추가

---
+++ scoreboard/src/components/Stopwatch.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+class Stopwatch extends Component {
+  tickRef;
+
+  state = {
+    isRunning: false,
+    timer: 0
+  };
+
+  componentDidMount() {
+    this.tickRef = setInterval(this.tick, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.tickRef);
+  }
+
+  tick = () => {
+    // isRunning이 true이면 timer를 1씩 증가
+    if (this.state.isRunning) {
+      this.setState(prevState => ({
+        timer: prevState.timer + 1
+      }));
+    }
+  };
+
+  handleStopwatch = () => {
+    this.setState(prevState => ({ isRunning: !prevState.isRunning }));
+  };
+
+  handleReset = () => {
+    this.setState({ timer: 0 });
+  };
+
+  render() {
+    return (
+      <div className="stopwatch">
+        <h2>Stopwatch</h2>
+        <span className="stopwatch-time">{this.state.timer}</span>
+        <button onClick={this.handleStopwatch}>{this.state.isRunning ? 'Stop' : 'Start'}</button>
+        <button onClick={this.handleReset}>Reset</button>
+      </div>
+    );
+  }
+}
+
+export default Stopwatch;
